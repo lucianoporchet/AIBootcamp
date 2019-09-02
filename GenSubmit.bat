f6a2d9d8d8386d51ae100ff4a30549c4b58272e3
AIGuru <carle.cote@gmail.com>
2019-09-01 20:55:30 -0400

    add genSubmit

---
+++ GenSubmit.bat
@@ -0,0 +1,8 @@
+@echo off
+
+rem =================================================>
+rem Update Configs Here
+set TEAM=LesOutardes
+rem <=================================================
+
+python GenSubmit.py %TEAM%
