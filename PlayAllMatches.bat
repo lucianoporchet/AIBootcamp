dc371c159b923f7b5e38f71a4a91f468724324a2
Alexandre Baud <baud.alexandre2@gmail.com>
2021-10-13 17:00:12 -0400

    Ajout execution plusieurs maps

---
+++ PlayAllMatches.bat
@@ -0,0 +1,28 @@
+@echo off
+
+set mapname=%1
+
+rem =================================================>
+rem Update Configs Here
+
+rem example : 
+rem set AIBOOTCAMP2_ENGINE_DIR=D:\AIBootcamp2\AIBootcamp2\Releases
+rem set AIBOT_PATH=D:\AIBootcamp2\LesOutardes\build-solution-x64\Debug\lesoutardes.dll
+rem set TEAM=LesOutardes
+
+set AIBOOTCAMP2_ENGINE_DIR=D:\AI\AIBootCamp2-2021\Releases
+set AIBOT_PATH=D:\AI\AIBootCamp\build-solution-x64\Debug\myfirstaibot.dll
+set TEAM=AlexEtLuciano
+
+rem in s, -1 = default
+set ConnectToProcessDelay=-1
+
+rem in ms, -1 = default
+set InitTime=-1
+set TurnTime=-1
+
+rem <=================================================
+
+python PlayAllMatches.py %mapname% %AIBOOTCAMP2_ENGINE_DIR% %AIBOT_PATH% %TEAM% %ConnectToProcessDelay% %InitTime% %TurnTime%
+
+exit /b
