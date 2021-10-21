c19f3268361edfaf62b0d16e3e9812c46f8c795f
Alexandre Baud <baud.alexandre2@gmail.com>
2021-10-21 10:29:21 -0400

    permier commit

--- HasPath.cpp
+++ HasPath.cpp
@@ -6,7 +6,7 @@ status HasPath::run(std::list<SOrder>&, int index, const STurnData& _turnData, L
 	Grid& grid = Grid::get();
 
 	if (grid.npc_states[index] == State::ARRIVED) return status::SUCCESS;
-	if (grid.List_of_paths.size() <= index) return status::FAILURE;
+	//if (grid.List_of_paths.size() <= index) return status::FAILURE;
 	if (!grid.List_of_paths[index].empty()) return status::SUCCESS;
 
 	return status::FAILURE;
--- PlayAllMatches.bat
+++ PlayAllMatches.bat
@@ -10,8 +10,8 @@ rem set AIBOOTCAMP2_ENGINE_DIR=D:\AIBootcamp2\AIBootcamp2\Releases
 rem set AIBOT_PATH=D:\AIBootcamp2\LesOutardes\build-solution-x64\Debug\lesoutardes.dll
 rem set TEAM=LesOutardes
 
-set AIBOOTCAMP2_ENGINE_DIR=C:\Users\porl2001\Desktop\AI\AIBootcamp2-2021\AIBootcamp2-2021\Releases
-set AIBOT_PATH=C:\Users\porl2001\Desktop\AI\AIBootCamp2-MyFirstAIBot-2021\AIBootCamp\build-solution-x64\Debug\myfirstaibot.dll
+set AIBOOTCAMP2_ENGINE_DIR=D:\AI\AIBootcamp2-2021\Releases
+set AIBOT_PATH=D:\AI\AIBootcamp\build-solution-x64\Debug\myfirstaibot.dll
 set TEAM=AlexEtLuciano
 
 rem in s, -1 = default
